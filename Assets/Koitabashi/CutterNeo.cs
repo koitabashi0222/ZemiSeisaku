6ec55f7a74fbf92e16a617bb5ed0e99d6ac49cff
koitabashi0222 <koita0222yuh@icloud.com>
2024-10-31 11:12:37 +0900

    cutterUpdate

--- Assets/Koitabashi/CutterNeo.cs
+++ Assets/Koitabashi/CutterNeo.cs
@@ -1,3 +1,4 @@
+using BLINDED_AM_ME;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,13 +19,13 @@ public class CutterNeo : MonoBehaviour
     // このコンポーネントを付けたオブジェクトのCollider.IsTriggerをONにする
     void OnTriggerEnter(Collider other)
     {
-        var meshCut = other.gameObject.GetComponent<MeshCutNeo>();
+        var meshCut = other.gameObject.GetComponent<MeshCut>();
         if (meshCut == null) { return; }
         //一方向のみで切断する方法、方向については適宜変更
         //var cutPlane = new Plane(transform.right, transform.position);
         //動きで切断する場合
         var cutPlane = new Plane (Vector3.Cross(transform.forward.normalized, prePos - transform.position).normalized, transform.position);
-        meshCut.Cut(cutPlane);
+        //meshCut.Cut(cutPlane);
     }
 
 }
